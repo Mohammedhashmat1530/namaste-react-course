7c6d7d6a9f519d0ba37b6c8454d1c382d6d5437d
hashmat1530 <hashmat2020vskp@gmail.com>
2024-02-09 20:40:43 +0530

    nested tags creation using react.createElement

--- inception/app.js
+++ inception/app.js
@@ -1,4 +1,13 @@
 
-const heading = React.createElement("h1", {id:"hello",style:{backgroundColor:"red",display:"none"}}, "Hello hustler!");
+const heading = React.createElement("h1", { id: "hello", style: { backgroundColor: "red", display: "none" } }, "Hello hustler!");
+
+const parent = React.createElement("div", { id: "parent" },React.createElement("div",{id:"child"},[React.createElement("h1",{},"Once a Hustler"),React.createElement("h1",{},"Is always a Hustler")]));
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+
+// wrap both heading and parent
+const app=React.createElement("div",{},[heading,parent]);
+root.render(app);
+
+
+

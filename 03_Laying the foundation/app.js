8461aef31de8cdc5c3dd5aee115e3228a0f732fa
hashmat1530 <hashmat2020vskp@gmail.com>
2024-02-12 16:12:14 +0530

    created basic foundation

---
+++ 03_Laying the foundation/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const heading = React.createElement("h1", { id: "hello", style: { backgroundColor: "red", display: "none" } }, "Hello hustler!");
+
+const parent = React.createElement("div", { id: "parent" },React.createElement("div",{id:"child"},[React.createElement("h1",{},"Once a Hustler"),React.createElement("h1",{},"Is always a Hustler")]));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// wrap both heading and parent
+const app=React.createElement("div",{},[heading,parent]);
+root.render(app);
+
+
+

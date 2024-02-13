2583bd2b5262232808f17afc86390150314e27b2
hashmat1530 <hashmat2020vskp@gmail.com>
2024-02-13 21:15:05 +0530

    done

--- 03_Laying the foundation/app.js
+++ 03_Laying the foundation/app.js
@@ -1,20 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
+// REact Element
 const heading = React.createElement("h1", { id: "hello", style: { backgroundColor: "red", display: "none" } }, "Hello hustler!");
-/*
-const parent = React.createElement("div", { id: "parent" },React.createElement("div",{id:"child"},[React.createElement("h1",{},"Once a Hustler"),React.createElement("h1",{},"Is always a Hustler")]));
-*/
+
 
 //JSX (transpiled before it reached the JS) - PARCEL - Babel
 const jsxHeading = <h1 id="hello">Hello Hustler hashmat  hsh            !</h1>
 
+//REact Component
+const ReactComponent1 = () => {
+    return <h1 id="hello " style={{ backgroundColor: 'red' }}>Hello Hustler ! hash from ReactComponent1</h1>
+}
+
+//React COmponent
+const ReactComponent2 = () => (
+
+    <div>
+        <h1 id="hello " style={{ backgroundColor: 'red' }}>Hello Hustler ! hash from ReactComponent2 </h1>
+        {jsxHeading}
+        <ReactComponent1/>
+    </div>
+)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// wrap both heading and parent
-const app=React.createElement("div",{},[heading,parent]);
-root.render(jsxHeading);
 
+
+root.render(<ReactComponent2 />);
 
 

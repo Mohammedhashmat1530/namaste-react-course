19f3c9e94675db389b2979c302de6e3aeecf782c
hashmat1530 <hashmat2020vskp@gmail.com>
2024-03-04 16:31:50 +0530

    commit lec-4'

--- 04_Talk is cheap,show me the code/app.js
+++ 04_Talk is cheap,show me the code/app.js
@@ -38,9 +38,77 @@ const Header =()=>{
     )
 }
 
+// Restaurant card component: Image, name, cuisine
+const RestaurantCard = () => {
+    return (
+      <div className="card">
+        <img
+          src={
+            "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+            'bz9zkh2aqywjhpankb07'
+          }
+        />
+        <h2>pizza</h2>
+        <h4>itilan</h4>
+        <h4>NAD</h4>
+        <span>
+          <h4>⭐5+</h4>
+          <h4>2.9kms</h4>
+          <h4>₹400 FOR TWO</h4>
+        </span>
+      </div>
+    )
+  }
+  
+  // Body Component for body section: It contain all restaurant cards
+  // We are mapping restaurantList array and passing data to RestaurantCard component as props with unique key as index
+  const Body = () => {
+    return (
+      <div className="restaurant-list">
+        
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        <RestaurantCard/>
+        
+
+      </div>
+    );
+  };
+  
+  // Footer component for footer section
+  const Footer = () => {
+    const year = new Date().getFullYear();
+    return (
+      <div className="footer">
+        Created By 💓
+        
+        <a className='linkedin' href="https://www.linkedin.com/in/mohammed-hashmat/" target="_blank">
+          Md Hashmat
+        </a>
+        ©️
+        {year}
+        <strong>
+          Food<span>Express</span>
+        </strong>
+      </div>
+    );
+  };
+
+
 const AppLayout = () => {
     return (
-        <Header/>
+        <React.Fragment>
+        <Header />
+        <Body />
+        <Footer />
+      </React.Fragment>
     )
 }
 
@@ -51,4 +119,4 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<AppLayout />);
 
-// 59:22
+// 1:35:36
